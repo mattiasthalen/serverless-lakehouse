0e94f2a517118d0f498c4ee0ee229b28a41c1365
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-13 21:54:56 +0100

    Update staging model generation

--- models/silver/stg__adventure_works__credit_cards.sql
+++ models/silver/stg__adventure_works__credit_cards.sql
@@ -1,34 +1,29 @@
 MODEL (
-    kind VIEW
+  kind VIEW
 );
 
-@DEF(primary_key, credit_card_id);
-
-WITH cte__source AS (
-    SELECT
-        *
-    FROM delta_scan("./lakehouse/bronze/raw__adventure_works__credit_cards")
-), cte__validity AS(
-    SELECT
-        *,
-        TO_TIMESTAMP(_dlt_load_id::DOUBLE) AS _sqlmesh__loaded_at,
-        ROW_NUMBER() OVER (PARTITION BY @primary_key ORDER BY _sqlmesh__loaded_at) AS _sqlmesh__version,
-        CASE
-            WHEN _sqlmesh__version = 1
-            THEN '1970-01-01 00:00:00'::TIMESTAMP
-            ELSE _sqlmesh__loaded_at
-        END AS _sqlmesh__valid_from,
-        COALESCE(
-            LEAD(_sqlmesh__loaded_at) OVER (PARTITION BY @primary_key ORDER BY _sqlmesh__loaded_at),
-            '9999-12-31 23:59:59'::TIMESTAMP
-        ) AS _sqlmesh__valid_to,
-        _sqlmesh__valid_to = '9999-12-31 23:59:59'::TIMESTAMP AS _sqlmesh__is_current,
-        CASE
-            WHEN _sqlmesh__is_current
-            THEN _sqlmesh__loaded_at
-            ELSE _sqlmesh__valid_to
-        END AS _sqlmesh__updated_at
-    
-    FROM cte__source
-)
-SELECT * FROM cte__validity
+SELECT
+   credit_card_id,
+   card_type,
+   card_number,
+   exp_month,
+   exp_year,
+   modified_date,
+   TO_TIMESTAMP(_dlt_load_id::DOUBLE) AS _sqlmesh__loaded_at,
+   ROW_NUMBER() OVER (PARTITION BY credit_card_id ORDER BY _sqlmesh__loaded_at) AS _sqlmesh__version,
+   CASE
+       WHEN _sqlmesh__version = 1
+       THEN '1970-01-01 00:00:00'::TIMESTAMP
+       ELSE _sqlmesh__loaded_at
+   END AS _sqlmesh__valid_from,
+   COALESCE(
+       LEAD(_sqlmesh__loaded_at) OVER (PARTITION BY credit_card_id ORDER BY _sqlmesh__loaded_at),
+       '9999-12-31 23:59:59'::TIMESTAMP
+   ) AS _sqlmesh__valid_to,
+   _sqlmesh__valid_to = '9999-12-31 23:59:59'::TIMESTAMP AS _sqlmesh__is_current,
+   CASE
+       WHEN _sqlmesh__is_current
+       THEN _sqlmesh__loaded_at
+       ELSE _sqlmesh__valid_to
+   END AS _sqlmesh__updated_at
+FROM delta_scan("./lakehouse/bronze/raw__adventure_works__credit_cards")
--- models/silver/stg__adventure_works__sales_order_details.sql
+++ models/silver/stg__adventure_works__sales_order_details.sql
@@ -1,34 +1,34 @@
 MODEL (
-    kind VIEW
+  kind VIEW
 );
 
-@DEF(primary_key, sales_order_detail_id);
-
-WITH cte__source AS (
-    SELECT
-        *
-    FROM delta_scan("./lakehouse/bronze/raw__adventure_works__sales_order_details")
-), cte__validity AS(
-    SELECT
-        *,
-        TO_TIMESTAMP(_dlt_load_id::DOUBLE) AS _sqlmesh__loaded_at,
-        ROW_NUMBER() OVER (PARTITION BY @primary_key ORDER BY _sqlmesh__loaded_at) AS _sqlmesh__version,
-        CASE
-            WHEN _sqlmesh__version = 1
-            THEN '1970-01-01 00:00:00'::TIMESTAMP
-            ELSE _sqlmesh__loaded_at
-        END AS _sqlmesh__valid_from,
-        COALESCE(
-            LEAD(_sqlmesh__loaded_at) OVER (PARTITION BY @primary_key ORDER BY _sqlmesh__loaded_at),
-            '9999-12-31 23:59:59'::TIMESTAMP
-        ) AS _sqlmesh__valid_to,
-        _sqlmesh__valid_to = '9999-12-31 23:59:59'::TIMESTAMP AS _sqlmesh__is_current,
-        CASE
-            WHEN _sqlmesh__is_current
-            THEN _sqlmesh__loaded_at
-            ELSE _sqlmesh__valid_to
-        END AS _sqlmesh__updated_at
-    
-    FROM cte__source
-)
-SELECT * FROM cte__validity
+SELECT
+   sales_order_id,
+   sales_order_detail_id,
+   carrier_tracking_number,
+   order_qty,
+   product_id,
+   special_offer_id,
+   unit_price,
+   unit_price_discount,
+   line_total,
+   rowguid,
+   modified_date,
+   TO_TIMESTAMP(_dlt_load_id::DOUBLE) AS _sqlmesh__loaded_at,
+   ROW_NUMBER() OVER (PARTITION BY sales_order_detail_id ORDER BY _sqlmesh__loaded_at) AS _sqlmesh__version,
+   CASE
+       WHEN _sqlmesh__version = 1
+       THEN '1970-01-01 00:00:00'::TIMESTAMP
+       ELSE _sqlmesh__loaded_at
+   END AS _sqlmesh__valid_from,
+   COALESCE(
+       LEAD(_sqlmesh__loaded_at) OVER (PARTITION BY sales_order_detail_id ORDER BY _sqlmesh__loaded_at),
+       '9999-12-31 23:59:59'::TIMESTAMP
+   ) AS _sqlmesh__valid_to,
+   _sqlmesh__valid_to = '9999-12-31 23:59:59'::TIMESTAMP AS _sqlmesh__is_current,
+   CASE
+       WHEN _sqlmesh__is_current
+       THEN _sqlmesh__loaded_at
+       ELSE _sqlmesh__valid_to
+   END AS _sqlmesh__updated_at
+FROM delta_scan("./lakehouse/bronze/raw__adventure_works__sales_order_details")
