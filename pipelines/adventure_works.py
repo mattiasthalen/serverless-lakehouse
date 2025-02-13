5a5bbaa37253390574ce52ffa67292b06719b8b0
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-13 14:30:02 +0100

    Add additional resources and land in lakehouse using delta

--- pipelines/adventure_works.py
+++ pipelines/adventure_works.py
@@ -1,5 +1,4 @@
 import dlt
-import os
 import typing as t
 
 from dotenv import load_dotenv
@@ -40,12 +39,70 @@ def adventure_works_source() -> t.Any:
                 "incremental": {
                     "start_param": "$filter",
                     "cursor_path": "ModifiedDate",
-                    "initial_value": "2014-01-01T00:00:00Z",
-                    "convert": lambda date: f"ModifiedDate gt {date}"
+                    "initial_value": "1970-01-01",
+                    "convert": lambda date: f"ModifiedDate gt {date}",
+                    "range_start": "open",
+                    "on_cursor_value_missing": "include"
                 },
             },
         },
         "resources": [
+            {
+                "name": "get__adventure_works__odata__v1__addresses",
+                "table_name": "raw__adventure_works__addresses",
+                "primary_key": "AddressId",
+                "endpoint": {
+                    "path": "/Addresses",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__credit_cards",
+                "table_name": "raw__adventure_works__credit_cards",
+                "primary_key": "CreditCardId",
+                "endpoint": {
+                    "path": "/CreditCards",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__currency_rates",
+                "table_name": "raw__adventure_works__currency_rates",
+                "primary_key": "CurrencyRateId",
+                "endpoint": {
+                    "path": "/CurrencyRates",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__customers",
+                "table_name": "raw__adventure_works__customers",
+                "primary_key": "CustomerId",
+                "endpoint": {
+                    "path": "/Customers",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__persons",
+                "table_name": "raw__adventure_works__persons",
+                "primary_key": "BusinessEntityId",
+                "endpoint": {
+                    "path": "/Persons",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__products",
+                "table_name": "raw__adventure_works__products",
+                "primary_key": "ProductId",
+                "endpoint": {
+                    "path": "/Products",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__product_categories",
+                "table_name": "raw__adventure_works__product_categories",
+                "primary_key": "ProductCategoryId",
+                "endpoint": {
+                    "path": "/ProductCategories",
+                },
+            },
             {
                 "name": "get__adventure_works__odata__v1__sales_order_details",
                 "table_name": "raw__adventure_works__sales_order_details",
@@ -62,6 +119,54 @@ def adventure_works_source() -> t.Any:
                     "path": "/SalesOrderHeaders",
                 },
             },
+            {
+                "name": "get__adventure_works__odata__v1__sales_persons",
+                "table_name": "raw__adventure_works__sales_persons",
+                "primary_key": "BusinessEntityId",
+                "endpoint": {
+                    "path": "/SalesPersons",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__sales_territories",
+                "table_name": "raw__adventure_works__sales_territories",
+                "primary_key": "TerritoryId",
+                "endpoint": {
+                    "path": "/SalesTerritories",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__ship_methods",
+                "table_name": "raw__adventure_works__ship_methods",
+                "primary_key": "ShipMethodId",
+                "endpoint": {
+                    "path": "/ShipMethods",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__special_offers",
+                "table_name": "raw__adventure_works__special_offers",
+                "primary_key": "SpecialOfferId",
+                "endpoint": {
+                    "path": "/SpecialOffers",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__state_provinces",
+                "table_name": "raw__adventure_works__state_provinces",
+                "primary_key": "StateProvinceId",
+                "endpoint": {
+                    "path": "/StateProvinces",
+                },
+            },
+            {
+                "name": "get__adventure_works__odata__v1__stores",
+                "table_name": "raw__adventure_works__stores",
+                "primary_key": "BusinessEntityId",
+                "endpoint": {
+                    "path": "/Stores",
+                },
+            },
         ],
     }
 
@@ -70,11 +175,9 @@ def adventure_works_source() -> t.Any:
 def load_adventure_works() -> None:
     load_dotenv()
     
-    duckdb_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "adventure_works.duckdb")
-    
     pipeline = dlt.pipeline(
         pipeline_name="adventure_works",
-        destination=dlt.destinations.duckdb(duckdb_path),
+        destination=dlt.destinations.filesystem("./lakehouse/Files"),
         dataset_name="bronze",
         progress="enlighten",
         export_schema_path="./pipelines/schemas/export",
@@ -84,7 +187,7 @@ def load_adventure_works() -> None:
 
     source = adventure_works_source()
     
-    load_info = pipeline.run(source)
+    load_info = pipeline.run(source, table_format="delta")
     print(load_info)
 
 if __name__ == "__main__":
