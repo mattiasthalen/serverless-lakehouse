d516cf90885287ed6900b67dce261263a1217d7e
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-14 14:25:23 +0100

    Refactor model generators

--- model_generators/generate_all_models.py
+++
@@ -1,7 +0,0 @@
-from generate_hook_bags import generate_hook_bags
-from generate_uss_bridges import generate_uss_bridges
-from generate_uss_bridge_union import generate_uss_bridge_union
-
-generate_hook_bags()
-generate_uss_bridges()
-generate_uss_bridge_union()
