eadbcf6d466bad75c516df419dba22fcb4984d0b
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-17 11:46:18 +0100

    Add sales order measures

--- models/silver/uss_bridge__sales_order_headers.sql
+++ models/silver/uss_bridge__sales_order_headers.sql
@@ -75,8 +75,68 @@ WITH bridge AS (
     ON bag__adventure_works__sales_order_headers._hook__ship_to_address = uss_bridge__addresses._hook__address
     AND bag__adventure_works__sales_order_headers.sales_order__record_valid_from <= uss_bridge__addresses.bridge__record_valid_to
     AND bag__adventure_works__sales_order_headers.sales_order__record_valid_to >= uss_bridge__addresses.bridge__record_valid_from
+), sales_order__order_date AS (
+    SELECT
+        bridge._pit_hook__sales_order,
+        bag__adventure_works__sales_order_headers.sales_order__order_date AS event_date,
+        1 AS measure__sales_order_placed
+    FROM bridge
+    INNER JOIN silver.bag__adventure_works__sales_order_headers USING (_pit_hook__sales_order)
+), sales_order__due_date AS (
+    SELECT
+        bridge._pit_hook__sales_order,
+        bag__adventure_works__sales_order_headers.sales_order__due_date AS event_date,
+        1 AS measure__sales_order_due,
+        CASE
+            WHEN bag__adventure_works__sales_order_headers.sales_order__ship_date = bag__adventure_works__sales_order_headers.sales_order__due_date
+            THEN 1
+        END AS measure__sales_order_shipped_on_time
+    FROM bridge
+    INNER JOIN silver.bag__adventure_works__sales_order_headers USING (_pit_hook__sales_order)
+), sales_order__ship_date AS (
+    SELECT
+        bridge._pit_hook__sales_order,
+        bag__adventure_works__sales_order_headers.sales_order__ship_date AS event_date,
+        1 AS measure__sales_order_shipped
+    FROM bridge
+    INNER JOIN silver.bag__adventure_works__sales_order_headers USING (_pit_hook__sales_order)
+), measures AS (
+    SELECT
+      bridge.*,
+      COALESCE(
+        sales_order__order_date.event_date,
+        sales_order__due_date.event_date,
+        sales_order__ship_date.event_date
+      ) AS event_date,
+      sales_order__order_date.* EXCLUDE (_pit_hook__sales_order, event_date),
+      sales_order__due_date.* EXCLUDE (_pit_hook__sales_order, event_date),
+      sales_order__ship_date.* EXCLUDE (_pit_hook__sales_order, event_date)
+    FROM bridge
+    FULL OUTER JOIN sales_order__order_date USING (_pit_hook__sales_order)
+    FULL OUTER JOIN sales_order__due_date USING (_pit_hook__sales_order, event_date)
+    FULL OUTER JOIN sales_order__ship_date USING (_pit_hook__sales_order, event_date)
 )
+
 SELECT
-  *,
+  stage,
+  _pit_hook__sales_order,
+  _hook__sales_order,
+  _pit_hook__credit_card,
+  _pit_hook__currency_rate,
+  _pit_hook__customer,
+  _pit_hook__sales_person,
+  _pit_hook__ship_method,
+  _pit_hook__address,
+  _pit_hook__state_province,
+  _pit_hook__territory,
+  CONCAT('calendar|date|', event_date)::BLOB AS _hook__calendar__date,
+  measure__sales_order_placed,
+  measure__sales_order_due,
+  measure__sales_order_shipped_on_time,
+  measure__sales_order_shipped,
+  bridge__record_loaded_at,
+  bridge__record_updated_at,
+  bridge__record_valid_from,
+  bridge__record_valid_to,
   bridge__record_valid_to = '9999-12-31 23:59:59'::TIMESTAMP AS bridge__is_current_record
-FROM bridge
+FROM measures
