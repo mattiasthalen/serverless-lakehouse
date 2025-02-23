db6d383e9b543db3cf2edcdb834941404ebfa3f0
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-23 09:30:23 +0100

    Remove unknown nulls from uss_bridge

--- models/silver/uss_bridge.sql
+++ models/silver/uss_bridge.sql
@@ -60,6 +60,7 @@ WITH bridge AS (
   FROM silver.uss_bridge__credit_cards
 )
 SELECT
+  stage,
   _pit_hook__address,
   _pit_hook__credit_card,
   _pit_hook__currency_rate,
@@ -87,3 +88,4 @@ SELECT
   bridge__record_valid_to,
   bridge__is_current_record
 FROM bridge
+WHERE stage IS NOT NULL
