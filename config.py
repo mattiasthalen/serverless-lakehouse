c026ef589becec499bf3fbfb9931ffb6e3a3c6bd
Mattias Thalén <bitter-polders0x@icloud.com>
2025-02-28 18:32:56 +0100

    Move back spark config to sqlmesh

--- config.py
+++ config.py
@@ -37,16 +37,26 @@ config = Config(
     project="adventure-works",
     default_target_environment=default_environment,
     gateways={
-            "spark": GatewayConfig(
-                connection=SparkConnectionConfig(
-                    config_dir="./",
-                    concurrent_tasks=1
-                ),
-                state_connection=DuckDBConnectionConfig(
-                    database="./lakehouse/state.duckdb",
-                )
+        "spark": GatewayConfig(
+            connection=SparkConnectionConfig(
+                config={
+                        "spark.jars.packages": "org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.8.0,org.xerial:sqlite-jdbc:3.43.0.0",
+                        "spark.sql.extensions": "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+                        "spark.sql.catalog.lakehouse": "org.apache.iceberg.spark.SparkCatalog",
+                        "spark.sql.catalog.lakehouse.type": "jdbc",
+                        "spark.sql.catalog.lakehouse.uri": "jdbc:sqlite:./lakehouse/catalog.db",
+                        "spark.sql.catalog.lakehouse.warehouse": "./lakehouse",
+                        "spark.sql.catalog.lakehouse.jdbc.schema-version": "V1",
+                        "spark.sql.defaultCatalog": "lakehouse",
+                        "spark.sql.warehouse.dir": "./lakehouse/spark-warehouse"
+                    },
+                concurrent_tasks=1
+            ),
+            state_connection=DuckDBConnectionConfig(
+                database="./lakehouse/state.duckdb",
             )
-        },
+        )
+    },
     default_gateway="spark",
     model_defaults=ModelDefaultsConfig(
         dialect="duckdb,normalization_strategy=case_sensitive",
